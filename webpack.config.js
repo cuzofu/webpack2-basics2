d3b62566d7ec11cf9caa721b192e9e272f29cace
cuzofu <cuzofu@qq.com>
2017-08-05 00:56:44 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+var extractTextPlugin = new ExtractTextPlugin({
+    filename: 'main.css'
+});
+
+module.exports = {
+    entry: {
+        app: './src/js/app.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        // publicPath: "/dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractTextPlugin.extract({
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                })
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]'
+                        }
+
+                    }
+                ],
+                exclude: path.resolve(__dirname, 'src/index.html')
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        extractTextPlugin,
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
+}
